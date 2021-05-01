2ac5e2f58acc0f490ff4fef0d5108259c9a7d0b1
Erno Kajcsa <kajcsaerno@gmail.com>
2021-05-01 15:51:35 +0200

    clean

--- springboot-backend/src/main/java/com/example/angularbackendapp/Application.java
+++ springboot-backend/src/main/java/com/example/angularbackendapp/Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.util.Arrays;
 import java.util.stream.Stream;
 
 @SpringBootApplication
@@ -19,13 +20,13 @@ public class Application {
     @Bean
     CommandLineRunner init(UserRepository userRepository) {
         return args -> {
-            Stream.of("John", "Julie", "Jennifer", "Helen", "Rachel", "Amelie").forEach(name -> {
+            for (String name : Arrays.asList("John", "Julie", "Jennifer", "Helen", "Rachel", "Amelie")) {
                 UserDTO userDTO = new UserDTO();
                 userDTO.setName(name);
                 userDTO.setAge((int) (Math.random() * ((100 - 1) + 1)));
                 userDTO.setEmail(name.toLowerCase() + "@domain.com");
                 userRepository.save(userDTO);
-            });
+            }
             userRepository.findAll().forEach(System.out::println);
         };
     }
